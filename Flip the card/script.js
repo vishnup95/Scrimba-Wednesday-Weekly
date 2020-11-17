fcb6730ba335f958bee25aebe55bd5fc40831cbc
vishnup95 <vishnu.wish7@gmail.com>
2020-11-17 23:16:34 +0530

    cleaned up flip card

--- Flip the card/script.js
+++ Flip the card/script.js
@@ -1,18 +1,67 @@
-/*
+const absolSection = document.getElementsByClassName("absol-details")[0];
+const megaAbsolSection = document.getElementsByClassName(
+  "mega-absol-details"
+)[0];
+const absolBg = document.getElementsByClassName('absol-bg')[0]
+const megaAbsolBg = document.getElementsByClassName('mega-absol-bg')[0]
 
-STRETCH GOALS:
+const pokemonDetails = ["absol", "absol-mega"]; // Order matters
 
-1) Go all out with styles: fonts, sizes, backgrounds, transitions, colours! 
+const P = new Pokedex.Pokedex();
+//API to fetch.
+const fetchPokemon = async (poke) => {
+  return await P.getPokemonByName(poke).catch((err) => {
+    throw new Error(err);
+  });
+};
 
-2) Find a purpose for your card: business card, invitation to a party, email subscription, 
-a video reveal or a music album cover with a song player at the back. 
+async function getPokemon() {
+  return await Promise.all(
+    pokemonDetails.map(async (pokemon) => {
+      return await fetchPokemon(pokemon);
+    })
+  );
+}
 
-3) Depending on you purpose add some the relevant interactivity: 
-    - business card: links to your social media
-    - invitation to a party, email subscription: confirmation of the subscription 
-    - a video reveal/song card: start playing the video/song
+generateAbsolDetails = (data) => {
+  data.forEach((form) => {
+    // Before Doing anything we call one more API to fetch the species info
+    const { name, abilities, types, sprites } = form;
+    const detailSection = document.createElement("ul");
+    const nameElement = document.createElement("li");
+    const ablityElement = document.createElement("li");
+    const typeElement = document.createElement("li");
+    const sprite = document.createElement("img");
 
-*/
+    nameElement.classList.add(`${name}`);
+    ablityElement.classList.add("ability");
+    typeElement.classList.add("type");
+    sprite.classList.add("absol-image");
 
 
+    nameElement.innerText = `${name.toUpperCase()}`;
+    ablityElement.innerText = `${abilities[0].ability.name.toUpperCase()}`;
+    typeElement.innerText = `${types[0].type.name.toUpperCase()}`;
+    sprite.src = sprites.other["official-artwork"]["front_default"]
 
+    detailSection.appendChild(nameElement);
+    detailSection.appendChild(typeElement);
+    detailSection.appendChild(ablityElement);
+
+    name === pokemonDetails[0]
+      ? absolSection.appendChild(detailSection)
+      : megaAbsolSection.appendChild(detailSection);
+
+    name === pokemonDetails[0]
+      ? absolBg.appendChild(sprite)
+      : megaAbsolBg.appendChild(sprite);
+  });
+};
+
+async function init() {
+  // getThePokemonInfo!
+  const absolForms = await getPokemon();
+  generateAbsolDetails(absolForms);
+}
+
+init();
