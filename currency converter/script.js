ae1e4225ba90cf9d6a121b037c0ba8e1c41a59a6
vishnup95 <vishnu.wish7@gmail.com>
2021-03-13 22:49:19 +0530

    currency converter with API

--- currency converter/script.js
+++ currency converter/script.js
@@ -1,24 +1,55 @@
-// Instructions:
-
-// 1. Add the functionality to exchange one currency to another
-//(you can use a technology of your choice) 🤖
-// 2. Style the app 🎨 ----  Do this Please!!!!!
+// Add an API integration..
+import { currencyList } from './util.js';
 
 const exchangeBtn = document.getElementById('exchange-btn');
 const orginalCurrAmount = document.getElementById('original-currency-amount');
 const fromCurrency = document.getElementById('original-currency-unit');
 const toCurrency = document.getElementById('new-currency-unit');
-const exchangeRate = document.getElementById('exchange-rate');
+const exchangeRateEle = document.getElementById('exchange-rate');
 const outputText = document.getElementById('output-text');
 
-exchangeBtn.onclick = function (e) {
-	let fromCurrencyValue = fromCurrency.value;
-	let toCurrencyValue = toCurrency.value;
-	let quantity = orginalCurrAmount.value;
-	let exchangeValue = exchangeRate.value;
-	if (fromCurrencyValue && toCurrencyValue && quantity && exchangeValue) {
-		// We will do a conversion here...
-		let outputCurrencyAmount = quantity * exchangeValue;
-		outputText.innerHTML = `Your ${quantity} ${fromCurrencyValue} will be ${outputCurrencyAmount} ${toCurrencyValue}`;
+const API_URI = 'https://api.exchangeratesapi.io';
+
+const addOptions = (value, innerText, selectElement) => {
+	let optionEle = document.createElement('option');
+	optionEle.value = value;
+	optionEle.innerText = innerText;
+	selectElement.appendChild(optionEle);
+};
+
+const init = () => {
+	//loop through the list of currencies and add it as an option?
+	// adding a default seems good.
+	addOptions('', 'Please Select One', fromCurrency);
+	addOptions('', 'Please Select One', toCurrency);
+	currencyList.forEach((currency) => {
+		addOptions(currency.symbol, currency.name, fromCurrency);
+		addOptions(currency.symbol, currency.name, toCurrency);
+	});
+};
+
+const getExchangeData = async (fromCurrency, toCurrency) => {
+	const exchangeRequest = await fetch(
+		`${API_URI}/latest?base=${fromCurrency}&symbols=${toCurrency}`
+	);
+	const exchangeData = await exchangeRequest.json();
+	return exchangeData;
+};
+
+const calculateFinalAmount = (rate) => {
+	let finalAmount = orginalCurrAmount.value * rate;
+	outputText.innerHTML = `Your ${orginalCurrAmount.value} of ${fromCurrency.value} will be ${finalAmount.toFixed(2)} of ${toCurrency.value}`;
+};
+
+exchangeBtn.onclick = function () {
+	if (fromCurrency.value && toCurrency.value) {
+		//call the API
+		getExchangeData(fromCurrency.value, toCurrency.value).then((data) => {
+			let exchangeRateData = data.rates[toCurrency.value];
+			exchangeRateEle.value = exchangeRateData.toFixed(2);
+			calculateFinalAmount(exchangeRateData);
+		});
 	}
 };
+
+init();
